d34c6d8589ad1efc604ccf4635634db308ed0067
Brett <Brett.marshall@shaw.ca>
2021-02-26 00:22:52 -0500

    Lab 4 mistake finished

--- Lab 4/lab04.sql
+++ Lab 4/lab04.sql
@@ -1,34 +1,222 @@
 use classicmodels;
 
-select distinct orders.customerNumber, customerName, sum(orderdetails.quantityOrdered*orderdetails.priceEach) as total
-from customers
-join orders on orders.customerNumber = customers.customerNumber 
-join orderdetails on orders.orderNumber = orderdetails.orderNumber
-group by orders.customerNumber
-order by total desc
-;
+-- Question 1
+select
+	distinct orders.customerNumber,
+	customerName,
+	sum(orderdetails.quantityOrdered*orderdetails.priceEach) as total
+from
+	customers
+join orders on
+	orders.customerNumber = customers.customerNumber
+join orderdetails on
+	orders.orderNumber = orderdetails.orderNumber
+group by customers.customerNumber 
+order by
+	total desc ;
 
+-- Question 2
 -- rounded to 2 decimal points
-select distinct orders.customerNumber, customerName, round(avg(orderdetails.quantityOrdered*orderdetails.priceEach),2) as average_total
-from customers
-join orders on orders.customerNumber = customers.customerNumber 
-join orderdetails on orders.orderNumber = orderdetails.orderNumber
-group by orders.customerNumber
-order by average_total desc
+select round(avg(testtable.total), 2) as average_total
+from (select
+	distinct orders.customerNumber,
+	customerName,
+	sum(orderdetails.quantityOrdered*orderdetails.priceEach) as total
+from
+	customers
+join orders on
+	orders.customerNumber = customers.customerNumber
+join orderdetails on
+	orders.orderNumber = orderdetails.orderNumber
+group by
+	orders.customerNumber
+order by
+	total desc) as testtable;
+
+
+-- Question 3
+ select
+	productName,
+	buyPrice
+from
+	products
+where
+	buyPrice in (
+	select
+		max(buyPrice)
+	from
+		products) ;
+	
+-- Question 4
+ select
+	distinct orders.customerNumber,
+	customers.customerName as expensivetaste
+from
+	orderdetails
+join products on
+	products.productCode = orderdetails.productCode
+join orders on
+	orders.orderNumber = orderdetails.orderNumber
+join customers on
+	customers.customerNumber = orders.customerNumber
+where
+	buyPrice in (
+	select
+		max(buyPrice)
+	from
+		products) ;
+
+-- Question 5
+select
+	testtable.customerNumber,
+	testtable.expensivetaste,
+	sum(orderdetails.quantityOrdered*orderdetails.priceEach) as total
+from
+	(
+	select
+		distinct orders.customerNumber, customers.customerName as expensivetaste
+	from
+		orderdetails
+	join products on
+		products.productCode = orderdetails.productCode
+	join orders on
+		orders.orderNumber = orderdetails.orderNumber
+	join customers on
+		customers.customerNumber = orders.customerNumber
+	where
+		buyPrice in (
+		select
+			max(buyPrice)
+		from
+			products) ) as testtable
+join orders on
+	orders.customerNumber = testtable.customerNumber
+join orderdetails on
+	orderdetails.orderNumber = orders.orderNumber
+group by
+	testtable.customerNumber
+order by
+	total desc ;
+
+-- Question 6
+
+select
+	testtable.customerNumber,
+	testtable.expensivetaste,
+	sum(orderdetails.quantityOrdered*orderdetails.priceEach) as total
+from
+	(
+	select
+		distinct orders.customerNumber, customers.customerName as expensivetaste
+	from
+		orderdetails
+	join products on
+		products.productCode = orderdetails.productCode
+	join orders on
+		orders.orderNumber = orderdetails.orderNumber
+	join customers on
+		customers.customerNumber = orders.customerNumber
+	where
+		buyPrice in (
+		select
+			max(buyPrice)
+		from
+			products) ) as testtable
+join orders on
+	orders.customerNumber = testtable.customerNumber
+join orderdetails on
+	orderdetails.orderNumber = orders.orderNumber
+group by
+	testtable.customerNumber
+having
+	total > (
+	select
+		round(avg(testtable.total), 2) as average_total
+	from
+		(
+		select
+			distinct orders.customerNumber, customerName, sum(orderdetails.quantityOrdered*orderdetails.priceEach) as total
+		from
+			customers
+		join orders on
+			orders.customerNumber = customers.customerNumber
+		join orderdetails on
+			orders.orderNumber = orderdetails.orderNumber
+		group by
+			orders.customerNumber
+		order by
+			total desc) as testtable )
+order by
+	total desc ;
+
+-- Question 7
+
+create or replace view expensiveTasteCustomers as
+ select
+	distinct orders.customerNumber,
+	customers.customerName as expensivetaste
+from
+	orderdetails
+join products on
+	products.productCode = orderdetails.productCode
+join orders on
+	orders.orderNumber = orderdetails.orderNumber
+join customers on
+	customers.customerNumber = orders.customerNumber
+where
+	buyPrice in (
+	select
+		max(buyPrice)
+	from
+		products) ;
+		
+select
+	expensiveTasteCustomers.customerNumber,
+	expensiveTasteCustomers.expensivetaste,
+	sum(orderdetails.quantityOrdered*orderdetails.priceEach) as total
+from
+	expensiveTasteCustomers
+join orders on
+	orders.customerNumber = expensiveTasteCustomers.customerNumber
+join orderdetails on
+	orderdetails.orderNumber = orders.orderNumber
+group by
+	expensiveTasteCustomers.customerNumber
+order by
+	total desc ;
+	
+-- Question 8 
+create or replace view purchaseTotal as 
+select
+	distinct orders.customerNumber,
+	customerName,
+	sum(orderdetails.quantityOrdered*orderdetails.priceEach) as total
+from
+	customers
+join orders on
+	orders.customerNumber = customers.customerNumber
+join orderdetails on
+	orders.orderNumber = orderdetails.orderNumber
+group by customers.customerNumber 
+order by
+	total desc ;
+
+select purchasetotal.customerNumber, purchasetotal.customerName, total
+from purchasetotal
+where total > (select round(avg(testtable.total), 2) as average_total
+from (select
+	distinct orders.customerNumber,
+	customerName,
+	sum(orderdetails.quantityOrdered*orderdetails.priceEach) as total
+from
+	customers
+join orders on
+	orders.customerNumber = customers.customerNumber
+join orderdetails on
+	orders.orderNumber = orderdetails.orderNumber
+group by
+	orders.customerNumber
+order by
+	total desc) as testtable)
 ;
 
-select productName, buyPrice 
-from products
-where buyPrice in (select max(buyPrice) from products)
-;
-
-select productCode 
-from products
-where buyPrice in (select max(buyPrice) from products)
-;
-
-select customers.customerName 
-from products
-join orderdetails on orderdetails.productCode = products.productCode
-join orders on orders.orderNumber = orderdetails.orderNumber 
-;
