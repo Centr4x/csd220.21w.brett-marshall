2fc61c8f2a247b8d88cda78a5d36e064ac0291c3
Brett <Brett.marshall@shaw.ca>
2021-04-09 12:21:51 -0400

    Lab6a finished

---
+++ Lab 6/part1.sql
@@ -0,0 +1,15 @@
+CREATE USER lab6 @ 'localhost' IDENTIFIED BY 'mypassword';
+
+CREATE ROLE superuser;
+
+GRANT ALL ON *.* TO superuser;
+
+GRANT superuser TO lab6 @ 'localhost';
+
+CREATE USER lab6 @ '%' IDENTIFIED BY 'mypassword';
+
+GRANT SELECT ON *.* TO lab6 @ '%';
+
+DROP USER lab6 @ 'localhost';
+DROP USER lab6 @ '%';
+DROP ROLE superuser;
